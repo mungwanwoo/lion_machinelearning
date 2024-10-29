7b57d474fc2f7ccd7dc2f66d5dd570e86bf6481e
mungwanwoo <ansrhksdn999@gmail.com>
2024-10-29 15:04:07 +0900

    캘리포니아 집값

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 # streamlit에 쓸 파일
 # ml은 colab
 import streamlit as st
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+import pickle
 
 st.title("MachineLearning")
-st.header("주식 예측 프로그램")
-st.write("sadasd")
-#얀녕
+st.header("캘리포니아 집값 예측 프로그램")
+cols=[ 'MedInc','HouseAge','AveRooms','AveBedrms', 'Population','AveOccup','Latitude','Longitude' ]
+datas=[0]*8
+for idx,col in enumerate(cols):
+    datas[idx]=st.number_input(col)
+
+x=np.array(datas).reshape(1,-1)
+
+with open("rf_house.pickle", 'rb') as f:
+    rf_model = pickle.load(f)
+y=rf_model.predict(x)
+
+
+st.header("예측되는 집값은 {}입니다.".format(y))
